eb41076af039c8d2ae202c533baf7bb77bac67d4
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-09 22:39:04 -0300

    update Missao03.js

--- Missao03.js
+++ Missao03.js
@@ -1,2 +1,17 @@
-
+/** ⚔️ MISSÃO 3
+O Exame dos Aprendizes
+📜 História
+Na taverna da aldeia, Mira comenta sobre um problema.
+Alguns jovens de Brighthollow tentaram entrar na Ordem de Oxenhall, mas precisam passar por um teste.
+Cada aprendiz fez duas provas:
+uma de conhecimento arcano
+outra de controle de runas
+O resultado depende da média das duas notas.
+Mira entrega um pergaminho com números escritos.
+“Se puder calcular rapidamente quem passou, poderemos enviar apenas os aprovados para a capital.”
+🎯 Objetivo da missão
+Criar um programa que:
+receba duas notas
+calcule a média
+mostre o resultado **/
 //console.log(`${}`); \n
