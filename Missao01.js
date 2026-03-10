b21ede3891fbecdab02a6e7f76a667d0de5c41de
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-09 21:28:14 -0300

    update Missao01.js

--- Missao01.js
+++ Missao01.js
@@ -1,1 +1,15 @@
-
+/** ⚔️ MISSÃO 1
+A Pedra Rúnica do Destino
+📜 História
+Um camponês encontrou uma pedra estranha nas ruínas da floresta.
+A pedra possui apenas um número gravado.
+Mas Mestre Ardan explica algo curioso:
+“As runas antigas dividem os números em duas forças fundamentais:
+a Ordem dos Pares e o Caos dos Ímpares.”
+Se a pedra for identificada corretamente, ela revelará um símbolo escondido.
+Caso contrário… pode liberar uma energia instável.
+Os moradores observam em silêncio enquanto você analisa o número.
+🎯 Objetivo da missão
+Criar um programa que:
+receba um número
+descubra se ele é par ou ímpar **/
