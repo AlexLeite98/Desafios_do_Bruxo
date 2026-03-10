77972276fa5e2adcc8ffcb3417f9de6dbf73e640
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-09 22:06:40 -0300

    update Missao02.js

---
+++ Missao02.js
@@ -0,0 +1,16 @@
+/** ⚔️ MISSÃO 2
+O Recrutamento do Rei
+📜 História
+Enquanto você conversa com Ardan, um mensageiro real chega à aldeia trazendo notícias preocupantes.
+Criaturas começaram a aparecer nas estradas do reino.
+O rei ordenou que novas tropas fossem recrutadas imediatamente.
+Capitão Roderick está tentando organizar voluntários na praça da aldeia.
+O problema é que muitos jovens querem se alistar… mas nem todos têm idade suficiente.
+Roderick suspira ao vê-lo.
+“Bruxo, preciso de um método rápido para verificar quem pode entrar no exército.
+A regra é simples: apenas quem tem 18 anos ou mais pode lutar.”
+Se você conseguir criar um sistema de verificação, poderá ajudar a organizar o recrutamento.
+🎯 Objetivo da missão
+Criar um programa que:
+1 receba a idade de uma pessoa
+2 diga se ela pode se alistar ou não **/
